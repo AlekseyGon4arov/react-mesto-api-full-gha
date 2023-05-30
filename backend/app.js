ddcefaca9d44cf67b4e2b361dbd876141f6f45dc
Alexey Goncharov <g-aleksey88@yandex.ru>
2023-05-30 21:41:25 +0300

    добавляет cors

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const authRouter = require('./routes/auth');
 const router = require('./routes');
 const auth = require('./middlewares/auth');
@@ -9,6 +10,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+app.use(cors());
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
