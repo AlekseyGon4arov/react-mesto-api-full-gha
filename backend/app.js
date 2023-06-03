3b541cddf900b6d92f35ab4e4c196d5002b7df4e
Alexey Goncharov <g-aleksey88@yandex.ru>
2023-06-03 18:45:28 +0300

    feat: crash-test

--- backend/app.js
+++ backend/app.js
@@ -20,6 +20,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 app.use(authRouter);
 app.use(auth);
 app.use(router);
